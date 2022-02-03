0917b6ec7d279b8e479afe4dfb235bd1c7ae27d9
marouaneYATIME <marwaneyatime14@gmail.com>
2022-02-03 11:49:15 +0100

    "developper la class activite"

--- src/twisk/monde/Activite.java
+++ src/twisk/monde/Activite.java
@@ -2,4 +2,33 @@ package twisk.monde;
 
 public class Activite {
 
+    private int temps;
+    private int ecartTemps;
+
+
+    /**
+     * constructeur de class activite
+     * @param nom
+     */
+    public Activite(String nom){
+
+    }
+
+
+
+    /**
+     * constructure de class activite
+     * @param nom
+     * @param t
+     * @param e
+     */
+    public Activite(String nom, int t, int e){
+        this.ecartTemps = e;
+        this.temps = t;
+    }
+
+    public boolean estActivite(){
+        return true;
+    }
+
 }
