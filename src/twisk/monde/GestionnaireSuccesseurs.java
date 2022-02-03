02fb3efec0e2026d0a062dd124d83942658792f9
aitouali1u <marouane.ait-ouali5@etu.univ-lorraine.fr>
2022-02-03 11:29:27 +0100

    Initialisation du monde

--- src/twisk/monde/GestionnaireSuccesseurs.java
+++ src/twisk/monde/GestionnaireSuccesseurs.java
@@ -1,4 +1,12 @@
 package twisk.monde;
 
+import java.util.Iterator;
+
 public class GestionnaireSuccesseurs implements Iterable<Etape> {
+
+
+    @Override
+    public Iterator<Etape> iterator() {
+        return null;
+    }
 }
