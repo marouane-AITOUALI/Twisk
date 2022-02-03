6c690014c5c5d831a085a85f21d5c4f3a98b627f
aitouali1u <marouane.ait-ouali5@etu.univ-lorraine.fr>
2022-02-03 11:28:53 +0100

    Initialisation du monde

--- src/twisk/monde/Etape.java
+++ src/twisk/monde/Etape.java
@@ -1,4 +1,19 @@
 package twisk.monde;
 
-public class Etape {
+import java.util.Iterator;
+
+public abstract class Etape implements Iterable<Etape> {
+
+    private String nom;
+    private GestionnaireSuccesseurs
+
+    public Etape(String nom){
+        this.nom = nom;
+    }
+
+
+    @Override
+    public Iterator<Etape> iterator() {
+        return null;
+    }
 }
---
+++ src/twisk/monde/GestionnaireSuccesseurs.java
@@ -0,0 +1,4 @@
+package twisk.monde;
+
+public class GestionnaireSuccesseurs implements Iterable<Etape> {
+}
