f0899db3167a0b733fe086e3886c001621133a67
aitouali1u <marouane.ait-ouali5@etu.univ-lorraine.fr>
2022-04-06 10:53:46 +0200

    Client du monde

--- src/twisk/simulation/Client.java
+++ src/twisk/simulation/Client.java
@@ -1,5 +1,20 @@
 package twisk.simulation;
 
+import twisk.monde.Etape;
+
 public class Client {
+
+    private int numeroClient;
+    private int rang;
+    private Etape etape;
+
+    public Client(int numero){
+        numeroClient = numero;
+    }
+
+    public void allerA(Etape etape, int rang){
+        this.etape = etape;
+        this.rang = rang;
+    }
 
 }
