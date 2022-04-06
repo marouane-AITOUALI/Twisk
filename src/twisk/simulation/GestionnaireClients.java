7944326b1701be795a955cbab380d724d2d5c8cb
aitouali1u <marouane.ait-ouali5@etu.univ-lorraine.fr>
2022-04-06 11:01:40 +0200

    Ajout du gestionnaire des clients

--- src/twisk/simulation/GestionnaireClients.java
+++ src/twisk/simulation/GestionnaireClients.java
@@ -1,4 +1,39 @@
 package twisk.simulation;
 
-public class GestionnaireClients {
+import twisk.monde.Etape;
+import twisk.monde.GestionnaireEtapes;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class GestionnaireClients implements Iterable<Client> {
+
+    private ArrayList<Client> clients = new ArrayList<>();
+
+    public GestionnaireClients(){
+
+    }
+
+    public GestionnaireClients(int nbClients){
+
+    }
+
+    public void setClients(int ... tabClients){
+        for(int pid: tabClients){
+            clients.add(new Client(pid));
+        }
+    }
+
+    public void allerA(int numeroClient, Etape etape, int rang){
+
+    }
+
+    public void nettoyer(){
+
+    }
+
+    public Iterator<Client> iterator(){
+        return clients.iterator();
+    }
+
 }
