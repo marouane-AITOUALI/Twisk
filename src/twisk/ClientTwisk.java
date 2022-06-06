ec6162dd076a261cf80e3b950fb420b88e39645f
aitouali1u <marouane.ait-ouali5@etu.univ-lorraine.fr>
2022-06-06 14:28:01 +0200

    Simulation deux mondes classLoaderPerso Introspection ClientTwisk

--- src/twisk/ClientTwisk.java
+++ src/twisk/ClientTwisk.java
@@ -11,66 +11,63 @@ import java.lang.reflect.Method;
 import java.util.Iterator;
 
 public class ClientTwisk {
-    private Monde monde;
+
     public ClientTwisk(){
-        this.monde = new Monde();
 
-        ClassLoaderPerso classLoaderPerso = new ClassLoaderPerso(monde.getClass().getClassLoader());
-
-        try {
-            Class<?> simulation = classLoaderPerso.loadClass("twisk.simulation.Simulation");
-
-            Constructor<?> constructor = simulation.getConstructor();
-            Object instance = constructor.newInstance();
-
-            Method setNbClients = simulation.getMethod("setNbClients", int.class);
-            Method simuler = simulation.getMethod("simuler", Monde.class);
-
-            setNbClients.invoke(instance, 4);
-            simuler.invoke(instance, monde);
-        } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException |
-                 InvocationTargetException e) {
-            throw new RuntimeException(e);
-        }
     }
     public static void main(String []args){
-
-        ClientTwisk clientTwisk = new ClientTwisk();
-
-
-
-
-        /*Activite zoo = new Activite("balade au zoo", 3, 1);
-        Guichet guichet = new Guichet("accès au toboggan", 2);
-        Activite tob = new ActiviteRestreinte("toboggan", 2, 1);
-
-        zoo.ajouterSuccesseur(guichet);
-        guichet.ajouterSuccesseur(tob);
-
-        monde.ajouter(zoo, tob, guichet);
-
-        monde.aCommeEntree(zoo);
-        monde.aCommeSortie(tob);
-
-
-        //s.setNbClients(5);
-
-
-
-        /*Monde monde2 = new Monde();
-
+        Monde monde = new Monde();
         Activite billiard = new Activite("billiard",2,1);
         Activite shop = new Activite("Acheter",3,1);
-        Guichet ticket = new Guichet("Ticket", 2);
+        Guichet ticket = new Guichet("G_Ticket", 2);
         Activite cinema = new ActiviteRestreinte("Cinema", 2, 1);
 
         billiard.ajouterSuccesseur(shop);
         shop.ajouterSuccesseur(ticket);
         ticket.ajouterSuccesseur(cinema);
 
-        monde2.ajouter(billiard, shop, ticket, cinema);
-        monde2.aCommeEntree(billiard);
-        monde2.aCommeSortie(cinema);*/
+        monde.ajouter(billiard, shop, ticket, cinema);
+        monde.aCommeEntree(billiard);
+        monde.aCommeSortie(cinema);
+
+        Monde monde2 = new Monde();
+        Activite billiard1 = new Activite("piscine",2,1);
+        Activite shop1 = new Activite("toboggan",3,1);
+        Guichet ticket1 = new Guichet("acces_tob", 2);
+        Activite cinema1 = new ActiviteRestreinte("toupie", 2, 1);
+
+        billiard1.ajouterSuccesseur(shop1);
+        shop1.ajouterSuccesseur(ticket1);
+        ticket1.ajouterSuccesseur(cinema1);
+
+        monde2.ajouter(billiard1, shop1, ticket1, cinema1);
+        monde2.aCommeEntree(billiard1);
+        monde2.aCommeSortie(cinema1);
+
+
+
+        try {
+            ClassLoaderPerso classLoaderPerso = new ClassLoaderPerso(ClientTwisk.class.getClassLoader());
+            Class<?> simulation = classLoaderPerso.loadClass("twisk.simulation.Simulation");
+
+            //Constructor<?> constructor = simulation.getConstructor();
+            Object instance = simulation.getDeclaredConstructor().newInstance();
+
+            Method setNbClients = simulation.getDeclaredMethod("setNbClients", int.class);
+            Method simuler = simulation.getDeclaredMethod("simuler", Monde.class);
+
+            setNbClients.invoke(instance, 4);
+            simuler.invoke(instance, monde);
+            simuler.invoke(instance, monde2);
+
+        }
+        catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException |
+               InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
 
         //Simulation s = new Simulation();
         //s.setNbClients(5);
